023c0e4d92ace192b07540a279c636a3087b1743
karthik-esper <kgangireddy04@gmail.com>
2023-11-01 16:12:05 -0400

    more stuff

--- src/main/java/com/banking/TransactionManagerMain.java
+++ src/main/java/com/banking/TransactionManagerMain.java
@@ -11,7 +11,7 @@ public class TransactionManagerMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TransactionManagerMain.class.getResource("TransactionManagerView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 700);
         stage.setTitle("Transaction Manager GUI");
         stage.setScene(scene);
         stage.show();
