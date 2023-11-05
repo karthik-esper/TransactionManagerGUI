1ab9b174889cb49d02252676530b4b9dc40ea2a8
karthik-esper <kgangireddy04@gmail.com>
2023-11-05 10:57:17 -0500

    reformatted the code order just to make things nicer

--- src/main/java/com/banking/TransactionManagerController.java
+++ src/main/java/com/banking/TransactionManagerController.java
@@ -254,33 +254,6 @@ public class TransactionManagerController {
         }
     }
 
-    private boolean validAge(String date) {
-        Date Test = new Date(date);
-        Date tooEarly = new Date ("11/6/2007");
-        Date tooLate = new Date ("11/6/1999");
-        if (tooEarly.compareTo(Test) == -1) {
-            openConsole.setText("DOB invalid: "  + date + " under 16.");
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean validAge(String date, String overload) {
-        Date Test = new Date(date);
-        Date tooEarly = new Date ("11/6/2007");
-        Date tooLate = new Date ("11/6/1999");
-        if (tooEarly.compareTo(Test) == -1) {
-            openConsole.setText("DOB invalid: "  + date + " under 16.");
-            return false;
-        }
-        else if (tooLate.compareTo(Test) == 1) {
-            openConsole.setText("DOB invalid: "  + date + " over 24.");
-            return false;
-        }
-        return true;
-    }
-
     private Account createAccount(Profile holder, double deposit) {
         if (getInitialDeposit() < 0) {
             openConsole.setText("Balance entered is either empty or invalid, please try again.");
@@ -416,8 +389,31 @@ public class TransactionManagerController {
         }
     }
 
+    private boolean validAge(String date) {
+        Date Test = new Date(date);
+        Date tooEarly = new Date ("11/6/2007");
+        Date tooLate = new Date ("11/6/1999");
+        if (tooEarly.compareTo(Test) == -1) {
+            openConsole.setText("DOB invalid: "  + date + " under 16.");
+            return false;
+        }
 
+        return true;
+    }
 
-
+    private boolean validAge(String date, String overload) {
+        Date Test = new Date(date);
+        Date tooEarly = new Date ("11/6/2007");
+        Date tooLate = new Date ("11/6/1999");
+        if (tooEarly.compareTo(Test) == -1) {
+            openConsole.setText("DOB invalid: "  + date + " under 16.");
+            return false;
+        }
+        else if (tooLate.compareTo(Test) == 1) {
+            openConsole.setText("DOB invalid: "  + date + " over 24.");
+            return false;
+        }
+        return true;
+    }
 
 }
